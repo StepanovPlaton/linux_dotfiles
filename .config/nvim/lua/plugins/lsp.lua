b8b56c70da11ec76fa96b49a562730980ee3cd37
StepanovPlaton <mail2platon@yandex.ru>
2023-12-26 02:54:34 +0400

    25.12.23 #2

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -43,89 +43,11 @@ return {
       })
     end
   },
-  {
-    "j-hui/fidget.nvim",
-    opts = {}
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-cmdline",
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then luasnip.expand_or_jump()
-            elseif has_words_before() then cmp.complete()
-            else fallback() end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then luasnip.jump(-1)
-            else fallback() end
-          end, { "i", "s" }),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        },
-        -- mapping = cmp.mapping.preset.insert({
-        --   ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        --   ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        --   ['<C-Space>'] = cmp.mapping.complete(),
-        --   ['<C-e>'] = cmp.mapping.abort(),
-        --   ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        -- }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        }, {
-          { name = 'buffer' },
-        })
-      })
-      cmp.setup.filetype('gitcommit', {
-        sources = cmp.config.sources(
-          {{ name = 'git' }}, 
-          {{ name = 'buffer' }}
-        )
-      })
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {{ name = 'buffer' }}
-      })
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources(
-          {{ name = 'path' }},
-          {{ name = 'cmdline' }}
-        )
-      })
-    end,
-  },
   { 
     "neovim/nvim-lspconfig",
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "williamboman/mason-lspconfig.nvim",
-      -- "ms-jpq/coq_nvim",
-      -- "ms-jpq/coq.artifacts",
-      -- "ms-jpq/coq.thirdparty"
       "hrsh7th/cmp-nvim-lsp",
     },
     keys = {
@@ -187,5 +109,9 @@ return {
         }
       )
     end,
+  },
+  {
+    "j-hui/fidget.nvim",
+    opts = {}
   },
 }
